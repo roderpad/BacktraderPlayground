e1a8803dfd178416f82c8a8e10f1e2ab00e8df59
Paden B. Roder <roderpad@gmail.com>
2021-08-07 14:31:51 -0700

    Update gitignore and change file structure

--- trader.py
+++ trader.py
@@ -1,6 +1,7 @@
+import os, sys, argparse
 import backtrader as bt
-import datetime
-from buyandhold import Strat
+import pandas as pd
+from Strats.buyandhold import Strat
 
 #* We need to connect a datafeed to a strategy for backtrader to do its thing. 
 cerebro = bt.Cerebro()
@@ -8,16 +9,22 @@ cerebro = bt.Cerebro()
 STARTINGCASH = 1000000 #* Define starting cash at $1M
 cerebro.broker.set_cash(STARTINGCASH)
 
-#* Create a Data Feed
-data = bt.feeds.YahooFinanceCSVData(
-    dataname='ORCL.csv', #* Oracle stock data from 1995-2014
-    # Do not pass values before this date
-    fromdate=datetime.datetime(2000, 1, 1),
-    # Do not pass values after this date
-    todate=datetime.datetime(2000, 12, 31),
-    reverse=False)
+#* Get prices from historical data
+orcl_prices = 'Data/ORCL.csv'
+spy_prices = pd.read_csv('Data/SPY.csv', index_col='Date', parse_dates=True)
 
-cerebro.adddata(data) #* Import data from feed
+#* Create Data Feeds
+# data = bt.feeds.YahooFinanceCSVData(
+#     dataname='ORCL.csv', #* Oracle stock data from 1995-2014
+#     # Do not pass values before this date
+#     fromdate=datetime.datetime(2000, 1, 1),
+#     # Do not pass values after this date
+#     todate=datetime.datetime(2000, 12, 31),
+#     reverse=False)
+ORCLyfFeed = bt.feeds.YahooFinanceCSVData(dataname=orcl_prices) #* Oracle stock data from 1995-2014, from Yahoo data download
+SPYyfFeed = bt.feeds.PandasData(dataname=spy_prices) #* S&P500 stock data from 2000-current, from Yahoo data download
+
+cerebro.adddata(ORCLyfFeed) #* Import ORCL data from feed
 
 cerebro.addstrategy(Strat) #* Import strategy to buy and hold from our strategy directory
 cerebro.addsizer(bt.sizers.FixedSize, stake=1000) #* Set order size to 1000 shares (default 1)
